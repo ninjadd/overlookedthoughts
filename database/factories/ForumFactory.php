b2d8ea592f38e3581e837048e618d8fe3fd9aeec
Daniel <ninjadd@gmail.com>
2021-06-12 16:45:51 -0700

    Forum framework in place now

---
+++ database/factories/ForumFactory.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Forum;
+use App\Models\User;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ForumFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Forum::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $userIds = User::all()->pluck('id');
+        $title = $this->faker->sentence('2');
+        $slug = Str::slug($title);
+
+        return [
+            'user_id' => $this->faker->randomElement($userIds),
+            'title' => $title,
+            'slug' => $slug,
+            'description' => $this->faker->paragraph(3)
+        ];
+    }
+}
